f60a0a3b153a7a599117683239c69b9948997d92
Utsav Kesharwani <utsav.kesharwani@gmail.com>
2016-10-02 03:04:40 +0530

    added more test cases for trips & tasks

--- spec/lib/resources/task_spec.rb
+++ spec/lib/resources/task_spec.rb
@@ -103,7 +103,7 @@ describe HyperTrack::Task do
       end
     end
 
-    context "required params missing for start" do
+    context "required params missing for complete" do
       it "should raise InvalidParameters error" do
         expect { HyperTrack::Task.new('task_id', {}).complete({}) }.
         to raise_error(HyperTrack::InvalidParameters, "Request is missing required params - [:completion_location]")
@@ -218,6 +218,143 @@ describe HyperTrack::Task do
         expect(task_list['count']).to eq(1)
         expect(task_list['results'][0]).to be_an_instance_of HyperTrack::Task
         expect(task_list['results'][0].id).to eq(@task_params[:id])
+      end
+    end
+  end
+
+  describe "HyperTrack::Task instance methods with valid args should work properly" do
+    before(:each) do
+      HyperTrack.secret_key = "abc"
+
+      @request_headers = {
+        Accept: '*/*',
+        'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        Authorization: "token #{HyperTrack.secret_key}",
+        'Content-Type' => 'application/json',
+        'User-Agent' => 'Ruby'
+      }
+
+      @task_params = {
+        id: "unique_task_id",
+        trip_id: nil,
+        destination_id: nil,
+        driver_id: nil,
+        action: "task",
+        status: "not_started",
+        eta: nil,
+        initial_eta: nil,
+        committed_eta: nil,
+        start_time: nil,
+        start_location: nil,
+        completion_time: nil,
+        completion_location: nil,
+        cancelation_time: nil,
+        order_id: nil,
+        tracking_url:  "http://eta.fyi/random-tracking-id",
+        tracking_urls: [
+          {
+            editable: "multiple",
+            number_of_edits: 0,
+            tracking_url: "http://eta.fyi/random-tracking-id"
+          }
+        ],
+        created_at: "2016-09-26T19:52:28.561076Z",
+        modified_at: "2016-09-26T19:52:28.561110Z",
+      }
+
+      @start_task_params = { start_location: { type: "Point", coordinates: [ 72.0, 19.0 ] }, start_time: Time.now.strftime("%Y-%m-%dT%H:%M") }
+      @complete_task_params = { completion_location: { type: "Point", coordinates: [ 72.0, 19.1 ] }, completion_time: Time.now.strftime("%Y-%m-%dT%H:%M") }
+      @cancel_task_params = { cancelation_time: Time.now.strftime("%Y-%m-%dT%H:%M") }
+      @update_dest_task_params = { location: { type: "Point", coordinates: [ 72.0, 18.0 ] } }
+    end
+
+    context "#editable_url with valid args" do
+      it "should return editable tracking url" do
+        @task_object = HyperTrack::Task.new(@task_params[:id], @task_params)
+
+        stub_request(:post, "#{HyperTrack::ApiClient::BASE_URL}/#{HyperTrack::ApiClient::API_VERSION}/#{HyperTrack::Task::API_BASE_PATH}#{@task_params[:id]}/editable_url/").
+          with({ body: { editable: 'multiple' }, headers: @request_headers }).
+          to_return({ body: {
+            tracking_url: "http://eta.fyi/Gb7Kp5",
+            editable: "multiple",
+            number_of_edits: 0
+          }.to_json, status: 200 })
+
+        new_editable_url = @task_object.editable_url(editable: "multiple")
+
+        expect(new_editable_url).not_to be_an_instance_of HyperTrack::Task
+      end
+    end
+
+    context "#start with valid args" do
+      it "should mark the start of the task" do
+        @task_object = HyperTrack::Task.new(@task_params[:id], @task_params)
+
+        stub_request(:post, "#{HyperTrack::ApiClient::BASE_URL}/#{HyperTrack::ApiClient::API_VERSION}/#{HyperTrack::Task::API_BASE_PATH}#{@task_params[:id]}/start/").
+          with({ body: @start_task_params, headers: @request_headers }).
+          to_return({ body: @task_params.merge(@start_task_params).to_json, status: 200 })
+
+        updated_task_object = @task_object.start(@start_task_params)
+
+        expect(updated_task_object).to be_an_instance_of HyperTrack::Task
+        expect(updated_task_object.id).to eq(@task_params[:id])
+        expect(updated_task_object.start_time).to eq(@start_task_params[:start_time])
+        expect(Util.symbolize_keys(updated_task_object.start_location)).to eq(@start_task_params[:start_location])
+      end
+    end
+
+    context "#complete with valid args" do
+      it "should mark the end of the task" do
+        @task_object = HyperTrack::Task.new(@task_params[:id], @task_params.merge(@start_task_params))
+
+        stub_request(:post, "#{HyperTrack::ApiClient::BASE_URL}/#{HyperTrack::ApiClient::API_VERSION}/#{HyperTrack::Task::API_BASE_PATH}#{@task_params[:id]}/completed/").
+          with({ body: @complete_task_params, headers: @request_headers }).
+          to_return({ body: @task_params.merge(@start_task_params).merge(@complete_task_params).to_json, status: 200 })
+
+        updated_task_object = @task_object.complete(@complete_task_params)
+
+        expect(updated_task_object).to be_an_instance_of HyperTrack::Task
+        expect(updated_task_object.id).to eq(@task_params[:id])
+        expect(updated_task_object.start_time).to eq(@start_task_params[:start_time])
+        expect(Util.symbolize_keys(updated_task_object.start_location)).to eq(@start_task_params[:start_location])
+        expect(Util.symbolize_keys(updated_task_object.completion_location)).to eq(@complete_task_params[:completion_location])
+        expect(updated_task_object.completion_time).to eq(@complete_task_params[:completion_time])
+      end
+    end
+
+    context "#cancel with valid args" do
+      it "should mark the cancellation of the task" do
+        @task_object = HyperTrack::Task.new(@task_params[:id], @task_params.merge(@start_task_params))
+
+        stub_request(:post, "#{HyperTrack::ApiClient::BASE_URL}/#{HyperTrack::ApiClient::API_VERSION}/#{HyperTrack::Task::API_BASE_PATH}#{@task_params[:id]}/canceled/").
+          with({ body: @cancel_task_params, headers: @request_headers }).
+          to_return({ body: @task_params.merge(@start_task_params).merge(@cancel_task_params).to_json, status: 200 })
+
+        updated_task_object = @task_object.cancel(@cancel_task_params)
+
+        expect(updated_task_object).to be_an_instance_of HyperTrack::Task
+        expect(updated_task_object.id).to eq(@task_params[:id])
+        expect(updated_task_object.start_time).to eq(@start_task_params[:start_time])
+        expect(updated_task_object.completion_location).to be_nil
+        expect(updated_task_object.completion_time).to be_nil
+        expect(updated_task_object.cancelation_time).to eq(@cancel_task_params[:cancelation_time])
+      end
+    end
+
+    context "#update_destination with valid args" do
+      it "should update the destination of the task" do
+        @task_object = HyperTrack::Task.new(@task_params[:id], @task_params.merge(@start_task_params))
+
+        stub_request(:post, "#{HyperTrack::ApiClient::BASE_URL}/#{HyperTrack::ApiClient::API_VERSION}/#{HyperTrack::Task::API_BASE_PATH}#{@task_params[:id]}/update_destination/").
+          with({ body: @update_dest_task_params, headers: @request_headers }).
+          to_return({ body: @task_params.merge(start_location: @update_dest_task_params[:location]).to_json, status: 200 })
+
+        updated_task_object = @task_object.update_destination(@update_dest_task_params)
+
+        expect(updated_task_object).to be_an_instance_of HyperTrack::Task
+        expect(updated_task_object.id).to eq(@task_params[:id])
+        expect(Util.symbolize_keys(updated_task_object.start_location)).not_to eq(@start_task_params[:start_location])
+        expect(Util.symbolize_keys(updated_task_object.start_location)).to eq(@update_dest_task_params[:location])
       end
     end
   end
